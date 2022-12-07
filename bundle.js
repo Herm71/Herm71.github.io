7962e5b517310a13193b3e743532a002e002b00f
Jason Chafin <jchafin@ucsc.edu>
2022-12-07 15:16:40 -0800

    update

--- bundle.js
+++ bundle.js
@@ -1,1 +1,149 @@
-(()=>{var t={639:()=>{jQuery(document).ready((function(t){t(window).scroll((function(){t(this).scrollTop()<100?t(".topbutton").fadeOut(500):t(".topbutton").fadeIn(500)})),t(".topbutton").on("click",(function(){return t("html, body").animate({scrollTop:0},250),!1}))}))},762:()=>{jQuery(document).ready((function(t){var o=t("#main .row .omega"),e="lift";t(".content-inner-pages")[0]?o.addClass(e):o.removeClass(e)}))}},o={};function e(r){var n=o[r];if(void 0!==n)return n.exports;var a=o[r]={exports:{}};return t[r](a,a.exports,e),a.exports}e.n=t=>{var o=t&&t.__esModule?()=>t.default:()=>t;return e.d(o,{a:o}),o},e.d=(t,o)=>{for(var r in o)e.o(o,r)&&!e.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:o[r]})},e.o=(t,o)=>Object.prototype.hasOwnProperty.call(t,o),(()=>{"use strict";e(639),e(762)})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/back-to-top.js":
+/*!*******************************!*\
+  !*** ./src/js/back-to-top.js ***!
+  \*******************************/
+/***/ (() => {
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * @link https://premium.wpmudev.org/blog/back-to-top-button-wordpress/
+ */
+
+jQuery(document).ready(function ($) {
+  var offset = 100;
+  var speed = 250;
+  var duration = 500;
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < offset) {
+      $('.topbutton').fadeOut(duration);
+    } else {
+      $('.topbutton').fadeIn(duration);
+    }
+  });
+  $('.topbutton').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, speed);
+    return false;
+  });
+});
+
+/***/ }),
+
+/***/ "./src/js/sidebar-fix.js":
+/*!*******************************!*\
+  !*** ./src/js/sidebar-fix.js ***!
+  \*******************************/
+/***/ (() => {
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * @link https://premium.wpmudev.org/blog/back-to-top-button-wordpress/
+ */
+
+jQuery(document).ready(function ($) {
+  var targetClass = $("#main .row .omega");
+  var newClass = "lift";
+  if ($(".content-inner-pages")[0]) {
+    targetClass.addClass(newClass);
+  } else {
+    targetClass.removeClass(newClass);
+  }
+});
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _js_back_to_top_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/back-to-top.js */ "./src/js/back-to-top.js");
+/* harmony import */ var _js_back_to_top_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_js_back_to_top_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _js_sidebar_fix_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/sidebar-fix.js */ "./src/js/sidebar-fix.js");
+/* harmony import */ var _js_sidebar_fix_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_js_sidebar_fix_js__WEBPACK_IMPORTED_MODULE_1__);
+
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
